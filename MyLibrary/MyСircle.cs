e7aa5fc407aaa82ef8ab1cd9b79f0b88a92c1be4
Alexey <lmokhareva@gmail.com>
2022-07-18 15:09:31 +0300

    Add MyLibrary project

---
+++ MyLibrary/MyСircle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class MyСircle : ISquare
+    {
+        public double Radius { get; }
+
+        public MyСircle(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius));
+            }
+
+            Radius = radius;
+        }
+
+        public double MakeSquare()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ MyLibrary/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Triangle : ISquare
+    {
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+
+        public Triangle(double a, double b, double c)
+        {
+            if (a+b<c || a+c<b || c+b<a)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a));
+            }
+
+            A = a;
+            B = b;
+            C = c;
+              
+        }
+
+        public double MakeSquare()
+        {
+            double p = (A + B + C) / 2;
+
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public bool isTriangleRight()
+        {
+            if (A > B && A > C)
+            {
+                return A * A == B * B + C * C;
+            }
+            else if (B > A && B > C)
+            {
+                return B * B == A * A + C * C;
+            }
+            else
+            {
+                return C * C == A * A + B * B;
+            }
+        }
+    }
+}
